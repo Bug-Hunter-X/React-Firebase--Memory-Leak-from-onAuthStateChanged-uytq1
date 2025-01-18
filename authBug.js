879331cfe3a2636d967d28ecd3bc11394b4d8549
BugHunter-X <askzenai@gmail.com>
2025-01-18 11:10:06 +0100

    Add authBug.js file

---
+++ authBug.js
@@ -0,0 +1,16 @@
+const unsubscribe = onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+
+// unsubscribe from onAuthStateChanged when component unmounts
+return () => {
+  unsubscribe();
+};
